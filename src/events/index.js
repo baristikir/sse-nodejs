6ecd2f82878242ac28d304bb65e3fe6274a3ccfb
Baris Tikir <brstkr@icloud.com>
2021-06-06 20:42:19 +0200

    Prettier Formatting

--- src/events/index.js
+++ src/events/index.js
@@ -1,26 +1,26 @@
 /**
  * * Custom Event Emitter
  */
-const EventEmitter = require("events");
+const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 emitter.setMaxListeners(100000);
 // Listening on Event Emits => Emitting Middleware function
 const listen = (res) => {
-  emitter.on("update", () => {
-    const data = {
-      status: "New Update available!",
-    };
-    res.sendEventStreamData(data);
-  });
+	emitter.on('update', () => {
+		const data = {
+			status: 'New Update available!',
+		};
+		res.sendEventStreamData(data);
+	});
 };
 // Basic Trigger for only the update
 const trigger = () => {
-  emitter.emit("update");
+	emitter.emit('update');
 };
 // Basic Unsubscribe
 const clear = (res) => {
-  res.emit("close");
+	res.emit('close');
 };
 
 exports.listen = listen;
--- src/middlewares/useServerSentEvents.js
+++ src/middlewares/useServerSentEvents.js
@@ -1,28 +1,28 @@
 const useServerSentEventsMiddleware = (req, res, next) => {
-  // SSE related headers
-  const headers = {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-    "Access-Control-Allow-Origin": "*",
-    "X-Accel-Buffering": "no",
-  };
-  res.writeHead(200, headers);
-  res.flushHeaders();
+	// SSE related headers
+	const headers = {
+		'Content-Type': 'text/event-stream',
+		Connection: 'keep-alive',
+		'Cache-Control': 'no-cache',
+		'Access-Control-Allow-Origin': '*',
+		'X-Accel-Buffering': 'no',
+	};
+	res.writeHead(200, headers);
+	res.flushHeaders();
 
-  // Callback for sending any data
-  const sendEventStreamData = (data) => {
-    console.log("Received Data: %o", data);
-    const sseResponse = `data: ${JSON.stringify(data)}\n\n`;
-    res.write(sseResponse);
-  };
+	// Callback for sending any data
+	const sendEventStreamData = (data) => {
+		console.log('Received Data: %o', data);
+		const sseResponse = `data: ${JSON.stringify(data)}\n\n`;
+		res.write(sseResponse);
+	};
 
-  // Apply to res => allows to use res.sendEventStreamDate(...) anywhere after subscription
-  Object.assign(res, {
-    sendEventStreamData,
-  });
+	// Apply to res => allows to use res.sendEventStreamDate(...) anywhere after subscription
+	Object.assign(res, {
+		sendEventStreamData,
+	});
 
-  next();
+	next();
 };
 
 module.exports = useServerSentEventsMiddleware;
